fbea5f4db3dfbc14f3480b44a4654f290f442885
MendesMontanaro <montanaromendesalv@gmail.com>
2022-01-20 12:22:56 -0300

    Commit de arquivos
    arquivos

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		<title>HSTI Network</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" media="screen" />
+        <!-- Theme CSS -->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.css" media="screen" />
+	</head>
+	<body>
+        <div class="container">
+            <div class="menu-cab">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Epresa</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Clientes</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <div class="banner-inicio">
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img/img01.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/img02.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/img03.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+
+            </div>
+            <div class="corpo-topo">
+                Corpo topo
+            </div>
+            <div class="corpo-meio">
+                corpo meio
+            </div>
+            <div class="corpo-baixo">
+                corpo baixo
+            </div>
+            <div class="rodape">
+                rodape
+            </div>
+        </div>
+	</body>
+</html>
---
+++ teste.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+    <title>HSTI Network</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css" media="screen" />
+    <!-- Theme CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.css" media="screen" />
+</head>
+<body>
+
+
+
+<nav>
+    <div class="nav-sticky">
+        <!-- Side Navigation Button -->
+        <a id="owl-menu-trigger" href="#0"><span class="owl-menu-icon"></span></a>
+        <!-- Main Navigation -->
+        <div class="owl-contener fright clearfix">
+            <div class="cat-title"></div>
+            <ul class="owl-menu menu-content">
+
+                <!--=========== Single Menu ===============-->
+                <li class="single-dropdown">
+                    <a class="active" href="http://www.upsolucoes.net.br/">Home</a>
+                </li>
+                <!--=========== Single Menu ===============-->
+                <li class="single-dropdown">
+                    <a class="" href="http://www.upsolucoes.net.br/empresa">Empresa</a>
+                </li>
+                <!--=========== Single Menu ===============-->
+                <li class="single-dropdown"> <a class="" href="http://www.upsolucoes.net.br/servicos">Serviços</a>
+                    <ul class="owl-nav-list-single">
+
+
+                        <li class="single-dropdown"><a href="http://www.upsolucoes.net.br/servicos/conexoes">Conexões</a>
+                            <ul class="owl-nav-list-single">
+
+
+                                <li>
+                                    <a href="http://www.upsolucoes.net.br/servicos/conexoes/servico:cabeamento-estruturado">Cabeamento Estruturado</a>
+                                </li>
+
+
+                                <li>
+                                    <a href="http://www.upsolucoes.net.br/servicos/conexoes/servico:compartilhamento-de-postes">Compartilhamento de Postes</a>
+                                </li>
+
+
+
+
+                                <li>
+                                    <a href="http://www.upsolucoes.net.br/servicos/conexoes/servico:fibra-optica">Fibra Óptica</a>
+                                </li>
+
+
+                                <li>
+                                    <a href="http://www.upsolucoes.net.br/servicos/conexoes/servico:radio-enlace">Rádio Enlace</a>
+                                </li>
+
+
+                            </ul>
+                        </li>
+
+
+                        <li class="single-dropdown"><a href="http://www.upsolucoes.net.br/servicos/consultoria">Consultoria</a>
+                            <ul class="owl-nav-list-single">
+
+
+
+
+
+
+                                <li>
+                                    <a href="http://www.upsolucoes.net.br/servicos/consultoria/servico:consultoria-para-isps">Consultoria para ISPs</a>
+                                </li>
+
+
+
+
+
+
+                            </ul>
+                        </li>
+
+
+                    </ul>
+                </li>
+                <li class="single-dropdown">
+                    <a class="" href="http://www.upsolucoes.net.br/clientes">Clientes</a>
+                </li>
+                <li class="single-dropdown">
+                    <a class="" href="http://www.upsolucoes.net.br/noticias">Notícias</a>
+                </li>
+                <li class="single-dropdown">
+                    <a class="" href="http://www.upsolucoes.net.br/contato">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
